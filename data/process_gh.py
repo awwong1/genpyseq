17f589d91e347f11ce4f1a0d5951eca38feac967
Alexander Wong <alex@udia.ca>
2019-04-23 14:02:12 -0600

    new process function

--- data/process_gh.py
+++ data/process_gh.py
@@ -7,6 +7,7 @@ import json
 import os
 import math
 import numpy as np
+from argparse import ArgumentParser
 from ast import parse
 from datetime import datetime
 from io import StringIO
@@ -19,6 +20,7 @@ logger = logging.getLogger("genpyseq")
 
 # possible characters to evaluate
 VALID_CHAR_IDS = (9, 10, ) + tuple(range(32, 127))
+
 
 def process_sample_file(gh_extract_line, tab="    "):
     try:
@@ -63,7 +65,7 @@ def process_sample_file(gh_extract_line, tab="    "):
         return None, "", []
 
 
-def main():
+def main(charseq_out="", tokenseq_out=""):
     logger.addHandler(logging.StreamHandler(sys.stdout))
     logger.setLevel(logging.INFO)
 
@@ -80,37 +82,45 @@ def main():
     for file_name in files_to_evaluate:
         base_name = os.path.basename(file_name)
         with open(file_name, "r") as f:
-            with Pool() as p:
-                it = p.imap(process_sample_file, f)
-                for result in it:
-                    file_id, content, tokens = result
-                    if file_id is not None and content and len(tokens):
-                        # evaluated_files[file_id] = {
-                        #     "content": content,
-                        #     "tokens": tokens
-                        # }
-                        num_files += 1
-                        num_chars += len(content)
-                        num_tokens += len(tokens)
-                        logger.info("{} ({}) f: {} c: {} t: {}".format(base_name, datetime.now() - start, num_files, num_chars, num_tokens))
-                        file_chars = (chr(2), ) + tuple(content) + (chr(3),)
-                        files_chars.append(file_chars)
-                        files_tokens.append(tokens)
-                    if num_files >= 5000:
-                        break
-        if num_files >= 5000:
+            # with Pool() as p:
+            #     it = p.imap(process_sample_file, f)
+            for line in f:
+                result = process_sample_file(line)
+                # for result in it:
+                file_id, content, tokens = result
+                if file_id is not None and content and len(tokens):
+                    # evaluated_files[file_id] = {
+                    #     "content": content,
+                    #     "tokens": tokens
+                    # }
+                    num_files += 1
+                    num_chars += len(content)
+                    num_tokens += len(tokens)
+                    logger.info("{} ({}) f: {} c: {} t: {}".format(
+                        base_name, datetime.now() - start, num_files, num_chars, num_tokens))
+                    file_chars = (chr(2), ) + tuple(content) + (chr(3),)
+                    files_chars.append(file_chars)
+                    files_tokens.append(tokens)
+                if num_files >= 1000:
+                    break
+        if num_files >= 1000:
             break
 
         # evaluated_path = os.path.join(dir_path, "gh_processed", base_name)
         # with open(evaluated_path, "w") as f:
         #     json.dump(evaluated_files, f)
         # evaluated_files = {}
-    with open("charseqs.json", "w") as f:
-        json.dump(files_chars, f, separators=(',',':'))
-    with open("tokenseqs.json", "w") as f:
-        json.dump(files_tokens, f, separators=(",",":"))
-
+    with open(charseq_out, "w") as f:
+        json.dump(files_chars, f, separators=(',', ':'))
+    with open(tokenseq_out, "w") as f:
+        json.dump(files_tokens, f, separators=(",", ":"))
 
 
 if __name__ == "__main__":
-    main()
+    parser = ArgumentParser()
+    parser.add_argument("--charseq-out", help="output of char seq file",
+               type=str, default="charseqs.json")
+    parser.add_argument("--tokenseq-out", help="output of token seq file",
+               type=str, default="tokenseqs.json")
+    args = parser.parse_args()
+    main(charseq_out=args.charseq_out, tokenseq_out=args.tokenseq_out)
