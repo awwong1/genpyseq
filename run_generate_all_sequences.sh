77fef63beb3732bd2a650e725f6ddaaeae70b3cd
Alexander Wong <alex@udia.ca>
2019-04-24 15:04:53 -0600

    added token hiddensize in shell script

--- run_generate_all_sequences.sh
+++ run_generate_all_sequences.sh
@@ -9,17 +9,14 @@ notica() { curl --data "d:$*" "https://notica.us/?aF7rC-Ou" ; }
 mkdir -p raw_gen_seq
 
 # Generate 10,000 character files, 1000 per temperature step
+# and generate 10,000 token files, 1000 per temperature step
 for temperature in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
 do
     echo "python3 main.py char --generate --disable-cuda --max-generate-len 50000 --temperature $temperature --generator-output-file raw_gen_seq/char_temperature$temperature.json"
-    python3 main.py char --generate --max-generate-len 50000 --temperature $temperature --generator-output-file raw_gen_seq/char_temperature$temperature.json
+    python3 main.py char --generate --disable-cuda --max-generate-len 50000 --temperature $temperature --generator-output-file raw_gen_seq/char_temperature$temperature.json
     notica "DONE char $temperature"
-done
 
-# Generate 10,000 token files, 1000 per temperature step
-for temperature in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0
-do
-    echo "python3 main.py token --generate --disable-cuda --max-generate-len 20000 --temperature $temperature --generator-output-file raw_gen_seq/token_temperature$temperature.json"
-    python3 main.py token --generate --max-generate-len 20000 --temperature $temperature --generator-output-file raw_gen_seq/token_temperature$temperature.json
+    echo "python3 main.py token --generate --disable-cuda --hidden-size 128 --max-generate-len 20000 --temperature $temperature --generator-output-file raw_gen_seq/token_temperature$temperature.json"
+    python3 main.py token --generate --disable-cuda --hidden-size 128 --max-generate-len 20000 --temperature $temperature --generator-output-file raw_gen_seq/token_temperature$temperature.json
     notica "DONE token $temperature"
 done
